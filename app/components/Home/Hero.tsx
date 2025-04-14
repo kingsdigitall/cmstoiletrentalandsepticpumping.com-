109999e2b82ad84b3cf5fde9107b84193d53f19e
Satnam-Gill <ersatnamgill@gmail.com>
2025-04-14 16:51:18 +0530

    (Update: Metas and content)

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
-    name: "Middletown Water Heater Pros",
+    name: `${ContactInfo.name}`,
     image:
       "https://ik.imagekit.io/serviceproviders/cmstoiletrentalandsepticpumping.com/logo.png?updatedAt=1744090675253",
     "@id": `${ContactInfo.baseUrl}`,
